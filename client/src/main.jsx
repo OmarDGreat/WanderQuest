a79144045330b61766761cb7667d60a31c2b4c7c
Omar <Omar.Daghestani@colorado.edu>
2025-02-28 18:18:15 -0700

    main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,8 @@ import App from './App';
 import './index.css';
 import axios from 'axios';
 
-// Set axios defaults
-axios.defaults.baseURL = 'http://localhost:5000/api';
+// Set axios defaults based on environment
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
